d6ddb3150c53d3b3e9bdd6dcb29349ae6442ae76
rasel mridha <raselmridha792@gmail.com>
2024-10-21 21:31:25 +0600

    preparing files with adding tailwind in vite

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-6xl font-bold'>React knowledge cafe</h1>            
+        </div>
+    );
+};
+
+export default Header;
